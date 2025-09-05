b56bce8e6571aa45de973555693f095af6e06f11
Baikhyunjun <efc3825@konkuk.ac.kr>
2025-09-05 17:09:33 +0900

    First Commit

---
+++ src/main/java/SpringX3/bulletin/board/web/BulletinBoardWebApplication.java
@@ -0,0 +1,13 @@
+package SpringX3.bulletin.board.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinBoardWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulletinBoardWebApplication.class, args);
+	}
+
+}
